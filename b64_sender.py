0d73ba94a709f8faef4fe9fb26966bc743f279e9
paff <jj2jom@gmail.com>
2023-09-17 01:11:24 +0700

    add cap prop frame

--- b64_sender.py
+++ b64_sender.py
@@ -4,17 +4,20 @@ import cv2
 
 
 
-cam = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+camera.set(cv2.CAP_PROP_FPS, 30)
 
 r = redis.Redis(host="192.168.1.105",port=6379)
 
 while True:
     try:
-        ret, frame = cam.read()
+        ret, frame = camera.read()
         encoded, buf = cv2.imencode('.jpg', frame)
         image = base64.b64encode(buf)
         r.set("image",image)
     except KeyboardInterrupt:
-        cam.release()
+        camera.release()
         cv2.destroyAllWindows()
         break;
